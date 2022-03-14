eadc66aae751ab064fd97f8e5f1308968d7e5128
Diego Correa T <algoritmia@labormedia.cl>
2022-03-14 15:40:46 +0000

    tests

--- src/main.rs
+++ src/main.rs
@@ -23,29 +23,41 @@ impl MyStruct {
 }
 
 fn main() {
-    let my_new_instance= &mut MyStruct {
-        my_string: "World".into(),
-        my_uint32: 42,
-    };
-    println!("Check A: {:#?}", my_new_instance);
-    println!("Check B: {:#?}", my_new_instance.my_associated_function("Hello", 65));
 
-    fn lets_create_a_function<'a> (myself: &'a mut MyStruct) -> &'a MyStruct {
-        myself.my_string = myself.my_string.to_owned() + " Tomato".into();
-        myself.my_uint32 = myself.my_uint32 + 78;
-        myself
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn tests() {
+        let my_new_instance= &mut MyStruct {
+            my_string: "World".into(),
+            my_uint32: 42,
+        };
+        println!("Check A: {:#?}", my_new_instance);
+
+        println!("Check B: {:#?}", my_new_instance.my_associated_function("Hello", 65));
+
+        fn lets_create_a_function<'a> (myself: &'a mut MyStruct) -> &'a MyStruct {
+            myself.my_string = myself.my_string.to_owned() + " Tomato".into();
+            myself.my_uint32 = myself.my_uint32 + 78;
+            myself
+        }
+    
+        fn lets_create_a_second_function(myself: &mut MyStruct) -> &MyStruct {
+            myself
+        }
+    
+        my_new_instance.bind(lets_create_a_function);
+    
+        println!("Check C: {:#?}", my_new_instance);
+    
+        let my_curry = my_new_instance.curry(lets_create_a_second_function, &mut MyStruct { my_string: " Pineapple".into(), my_uint32: 0 });
+    
+        println!("Check D: {:#?}", my_curry());
+        // println!("Check D: {:#?}", my_curry())
     }
 
-    fn lets_create_a_second_function(myself: &mut MyStruct) -> &MyStruct {
-        myself
-    }
-
-    my_new_instance.bind(lets_create_a_function);
-
-    println!("Check C: {:#?}", my_new_instance);
-
-    let my_curry = my_new_instance.curry(lets_create_a_second_function, &mut MyStruct { my_string: " Pineapple".into(), my_uint32: 0 });
-
-    println!("Check D: {:#?}", my_curry());
-    // println!("Check D: {:#?}", my_curry())
+ 
 }
