26f86787d8cdde4acd659264fb7a064293848e09
Diego Correa T <algoritmia@labormedia.cl>
2023-01-10 16:53:11 +0000

    one category down

--- examples/iter_nesting.rs
+++ examples/iter_nesting.rs
@@ -1,28 +1,38 @@
 use functional_tests::apply;
 
-fn hourglass_sum(arr: &Vec<Vec<i32>>) -> i32 {
+fn hourglass_sum<'a>(arr: &'a Vec<Vec<i32>>) -> i32 {
     let arr_size = arr.len();
     let hg_last_index = 2;
-    let hg = |cursor:(usize, usize), data: &[Vec<i32>]| data[0+cursor.0..=hg_last_index+cursor.0]
+    let hg = |cursor:(usize, usize), data: &'a [Vec<i32>]| data[0+cursor.0..=hg_last_index+cursor.0]
         .iter()
         .enumerate()
-        .map( |(i,x)| 
+        .map( move |(i,x)| 
             x[cursor.1..=hg_last_index+cursor.1]
                 .iter()
                 .enumerate()
-                // .filter( |(j,_y)| {
-                //     !( i == 1_usize && (j == &(0_usize) || j == &(2_usize)) )
-                // })
+                .filter( |(j,_y)| {
+                    !( i == 1_usize && (j == &(0_usize) || j == &(2_usize)) )
+                })
                 .map( |(_j,y)| *y)
                 .collect::<Vec<i32>>()
-        )
-        .collect::<Vec<Vec<i32>>>();
-    let first_row_hg = (0..arr_size-hg_last_index)
+        );
+    let full_subset = (0..arr_size-hg_last_index)
         .map( |x| {
-            (0..arr_size-hg_last_index).map( |y| hg((x,y), arr) ).collect::<Vec<Vec<Vec<i32>>>>()
+            (0..arr_size-hg_last_index).map( |y| hg((x,y), arr)
+            .collect::<Vec<Vec<i32>>>() ).collect::<Vec<Vec<Vec<i32>>>>()
         } )
         .collect::<Vec<Vec<Vec<Vec<i32>>>>>();
-    println!("{:?}", first_row_hg);
+    let full_subset_flatten = (0..arr_size-hg_last_index)
+    .map( |x| {
+        (0..arr_size-hg_last_index).map( |y| 
+            hg((x,y), arr)
+                .flatten()
+                .collect::<Vec<i32>>() ).collect::<Vec<Vec<i32>>>()
+    } )
+    .collect::<Vec<Vec<Vec<i32>>>>();
+
+    println!("full_subset : {:?}", full_subset);
+    println!("full_subset_flatten : {:?}", full_subset_flatten);
     0
 }
 
